92ed4049bb4f50aa9cc54431ef00c8081132b2f8
Dungna89 <dungna29@fpt.edu.vn>
2021-09-13 11:28:35 +0700

    1

---
+++ 2_CP17201_MOB1014_FALL21/src/BAI_1_LAMQUENVOIJAVA/B1_ChuongTrinhDauTien.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BAI_1_LAMQUENVOIJAVA;
+
+/**
+ *
+ * @author Dungna89
+ */
+public class B1_ChuongTrinhDauTien {
+  //1. Chương trình để chạy được cần có 1 phương thức main
+  //2. psvm + tab: gọi nhanh main
+  //3. ctrl + space: gợi ý code
+  //4. main + tab: gọi nhanh main
+  //5. sout + tab: in ra màn hình
+  //6. shift + 6: chạy chương trình hoặc chuột phải vào file code cọn Run File
+  public static void main(String[] args) {
+    System.out.println("Chúc bạn Tú sẽ quan môn java 1");
+    System.out.println("Chào mừng các bạn đến với môn java 1");
+  }
+}
