8f4f2dd06b951c1b020c9f4f24372827ae44e4d1
Dungna89 <dungna29@fpt.edu.vn>
2021-09-15 11:27:05 +0700

    1

--- 2_CP17201_MOB1014_FALL21/src/BAI_1_LAMQUENVOIJAVA/B1_ChuongTrinhDauTien.java
+++ 2_CP17201_MOB1014_FALL21/src/BAI_1_LAMQUENVOIJAVA/B1_ChuongTrinhDauTien.java
@@ -10,14 +10,24 @@ package BAI_1_LAMQUENVOIJAVA;
  * @author Dungna89
  */
 public class B1_ChuongTrinhDauTien {
+
   //1. Chương trình để chạy được cần có 1 phương thức main
   //2. psvm + tab: gọi nhanh main
   //3. ctrl + space: gợi ý code
   //4. main + tab: gọi nhanh main
   //5. sout + tab: in ra màn hình
   //6. shift + 6: chạy chương trình hoặc chuột phải vào file code cọn Run File
+  //7. Ctrl + Shift + Mũi tên lên xuống: Copy nhanh 1 line code hoặc nhiều
+  //8. Ctrl + /: Comment code
+  //9. Alt + Shift + f: Format code trên file đang code (Clean Code)
   public static void main(String[] args) {
     System.out.println("Chúc bạn Tú sẽ quan môn java 1");
     System.out.println("Chào mừng các bạn đến với môn java 1");
+    //Bài 1: Viết 1 chương trình in ra các thông tin cá nhân: Tên, Tuổi, Ngành Học, Quê Quán trên từng dòng và in ra màn hình
+    System.out.println("Tú");
+    System.out.println("3x tuổi");
+    System.out.println("Mobile");
+    System.out.println("HCM");
+    
   }
 }
---
+++ 2_CP17201_MOB1014_FALL21/src/BAI_1_LAMQUENVOIJAVA/S2_KhaiBaoBien.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BAI_1_LAMQUENVOIJAVA;
+
+/**
+ *
+ * @author Dungna89
+ */
+public class S2_KhaiBaoBien {
+
+  /*
+      *********** Biến khái niệm cơ bản ***********
+      Định nghĩa: Dùng để lưu trữ giá trị trong lập trình. Biến được lưu trữ ở trong Ram.
+      Công thức: <kiểu dữ liệu> <tên biến> [=giá trị khởi đầu];
+      <kiểu dữ liệu>: Giúp định nghĩa giá trị muốn lưu trữ có kiểu dữ liệu là gì
+      <tên biến>:Giúp định danh và gọi biến đó khi sử dụng. Có phân biệt chữ hoa chữ thường.
+      [=giá trị khởi đầu]: Đưa giá trị đúng kiểu dữ liệu
+  
+      *********** Quy tắc đặt tên ***********
+      Tên biến có phân biệt chữ hoa và chữ thường. Nên đặt tên biến bằng Tiếng Anh
+         String firstname;// Kiểu Normal  //Sử dụng khi tên biến có 2 từ trở lên
+         String FirstName; //Kiểu CamelCase //Sử dụng khi tên Class có 2 từ trở lên hoặc 1 từ
+         String first_name; // Kiểu Underscores //Sử dụng khi tên biến có 2 từ trở lên 
+         Lưu ý: đặt tên biến không được phép bắt đầu bởi số , có dấu gạch ngang, sử dụng từ khóa của java (int,if,long....)
+         int int = 5; //Vi phạm nguyên tắc đặt biến
+         Trong cùng 1 class thì biến phải sử dụng tên khác nhau và đặt sao cho đúng nghĩa đại diện
+      *********** Các kiểu dữ liệu có trong java ***********  
+            + Kiểu số nguyên:byte, short, long, int
+            + Kiểu số thực:float, double
+            + Kiểu chuỗi và ký tự: String, char
+            + Kiểu Logic: boolean (true, false)
+            + Kiểu var: tự định nghĩa theo giá trị
+  
+      *********** Biến toàn cục và biến cục bộ ***********        
+      + Biến toàn bộ: Khai báo bên ngoài của phương thức
+        - Bắt buộc phải khai báo có dấu gạch dưới _ trước tên biến:
+        Ví dụ đặt tên: _firtName, _firstName;
+      + Biến cục bộ: Khai báo bên trong phương thức. Biến cục bộ sẽ chỉ được sử dụng ở bên trong của 1 phương thức.  
+   */
+  static int _temp;//Khai báo 1 biến toàn cục    
+
+  public static void main(String[] args) {
+    //1. Char kiểu ký tự
+    char c1 = 'a';
+    char c2 = '/';
+    char c3 = '1';
+    char c4 = '+';
+    char c5, c6, c7 = '4';
+
+    //2. String kiểu chuỗi
+    String s1 = "FPOLY";//Khai báo biến tên s1 có kiểu dữ liệu là string và có khởi tạo giá trị ban đầu.
+    String s2;//Khai báo biến tên s2 có kiểu dữ liệu là string và không khởi tạo giá trị ban đầu.
+    String s3, s4, s5 = "FPOLY";//Khai báo nhiều tên biến nhưng phải có kiểu cùng kiểu dữ liệu và có thể khởi tạo hoặc không khởi tạo giá trị ban đầu.
+
+    //3. int kiểu số nguyên
+    int a1 = 5;
+
+    //4. double, float kiểu số thực
+    double d1 = 9.6;
+    float f1 = 9.6f;//f có thể viết hoa hoặc viết thường
+
+    //5. boolean kiểu logic
+    boolean b1 = true;//Chỉ nhận 2 giá trị true và false
+
+    //6. var
+    //var name;//Khi sử dụng var bắt buộc phải khởi tạo giá trị
+    var v1 = "Fpoly";
+    var v2 = 1;
+    var v3 = 9.6;
+    var v4 = 'a';
+
+  }
+}
